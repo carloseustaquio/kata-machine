24581c23a9a71e329a023acd1d1e0f0cd9b87840
Carlos Eustáquio <43277022+carloseustaquio@users.noreply.github.com>
2024-02-29 13:34:42 +0000

    revisiting binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,24 +1,56 @@
+// ##### 3
 export default function bs_list(haystack: number[], needle: number): boolean {
-	let low = 0;
-	let high = haystack.length - 1;
+	let left = 0;
+	let right = haystack.length - 1;
 
-	while (low <= high) {
-		const m = Math.floor(low + (high - low) / 2);
-		const curr = haystack[m]
+	while (left <= right) {
+		const mid = Math.floor(((right - left) / 2) + left) // left is the offset and right-left is the actual mid point between the needles
+		const curr = haystack[mid];
 
 		if (curr === needle) return true;
+		if (left === right) return false;
 
-		if (high === low) return false;
-
-		if (curr > needle) {
-			high = m;
+		if (curr < needle) {
+			left = mid + 1;
 		} else {
-			low = m + 1;
+			right = mid;
 		}
 	}
 
 	return false;
 }
+
+// const result = bs_list([0, 1, 2, 3, 4, 5], 2);
+// console.log(result)
+
+// 0, 1, 2, 3, 4
+// l     m     r
+//       l     r
+
+// ####### 2
+// export default function bs_list(haystack: number[], needle: number): boolean {
+// 	let low = 0;
+// 	let high = haystack.length - 1;
+
+// 	while (low <= high) {
+// 		const m = Math.floor(low + (high - low) / 2);
+// 		const curr = haystack[m]
+
+// 		if (curr === needle) return true;
+
+// 		if (high === low) return false;
+
+// 		if (curr > needle) {
+// 			high = m;
+// 		} else {
+// 			low = m + 1;
+// 		}
+// 	}
+
+// 	return false;
+// }
+
+// ##### 1
 // export default function bs_list(haystack: number[], needle: number): boolean {
 // 	let low = 0;
 // 	let high = haystack.length;
